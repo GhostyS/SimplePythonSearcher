14ca7ec0204a229620897e037cfd161e0687938a
GhostyS <40345400+GhostyS@users.noreply.github.com>
2023-04-11 12:43:44 +0300

    Update methods.py

--- scripts/methods.py
+++ scripts/methods.py
@@ -1,15 +1,9 @@
 from app import db, es
-from app.elastic import delete_by_id, query_index_by_text, query_index_by_id
+import app.elastic_methods
 
 def search(Model, text):
-    for doc in Model.query.all():
-        add_to_index('docs', doc)
-    if text is None:
-        return "Text is None"
     try:
-        ids = query_index_by_text('docs', text)
-        result = Model.query.filter(Model.id.in_(ids)).order_by(Model.created_date.desc()).all()
-        return {"result": [doc.as_dict() for doc in result]}
+
     except Exception:
         return Exception
    
