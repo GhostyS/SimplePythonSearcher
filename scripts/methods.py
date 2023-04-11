729e4fa66642fe68af9078e46141c4e13a4e9bf2
GhostyS <40345400+GhostyS@users.noreply.github.com>
2023-04-11 12:26:14 +0300

    Update methods.py

--- scripts/methods.py
+++ scripts/methods.py
@@ -6,6 +6,9 @@ def search(Model, text):
         add_to_index('docs', doc)
     if text is None:
         return "Text is None"
-    ids = query_index_by_text('docs', text)
-    result = Model.query.filter(Model.id.in_(ids)).order_by(Model.created_date.desc()).all()
-    return {"result": [doc.as_dict() for doc in result]}
+    try:
+        ids = query_index_by_text('docs', text)
+        result = Model.query.filter(Model.id.in_(ids)).order_by(Model.created_date.desc()).all()
+        return {"result": [doc.as_dict() for doc in result]}
+    except Exception:
+        return Exception
