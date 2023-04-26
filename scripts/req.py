a956de72109f87f3eb4484553b4c0728eb3f815d
GhostyS <40345400+GhostyS@users.noreply.github.com>
2023-04-26 13:39:36 +0300

    Update req.py

--- scripts/req.py
+++ scripts/req.py
@@ -5,12 +5,19 @@ from app.tasks import search, delete
 
 @app.get('/search/')
 def get_posts():
-    #text = request.args["text"]
+    text = request.args["text"]
     result = loop.run_until_complete(search(Docs, text))
     return result
 
 @app.delete('/delete/')
 def delete_post():
-    id = request.args["id"]
+    delete_item = query_index_by_id('docs', id)
+    if delete_item != NULL:
+        elastic_result = delete_by_id('docs', id)
+        if elastic_result is True:
+            db_result = bool(Model.query.filter(Model.id==id).delete())
+            db.session.commit()
+            return str(db_result)
+        return "Not found post with such id"
     result = loop.run_until_complete(delete(Docs, id))
     return 
