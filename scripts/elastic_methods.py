e9798ae9a2b3652b3289e54fde2d11e8395f760e
GhostyS <40345400+GhostyS@users.noreply.github.com>
2023-05-03 21:51:37 +0300

    Update elastic_methods.py

--- scripts/elastic_methods.py
+++ scripts/elastic_methods.py
@@ -8,12 +8,14 @@ def delete_by_id(index, id):
     except elasticsearch.NotFoundError:
        return False
 
-def get_id_by_text(index, text):
-     try:
-        el_s.delete(index=index, id=id)
-        return True
-    except elasticsearch.NotFoundError:
-       return False
+def query_index_by_text(index, text):
+    search = es.search(
+        index=index,
+        size=20,
+        query = { 'multi_match': {'query': text, 'fields': ['*']}, "sort": [{"date_field": {"order": "desc"}}]})
+    ids = [int(hit['_id']) for hit in search['hits']['hits']]
+    return ids
+      
     
 
 def get_doc_by_id(index, id):
