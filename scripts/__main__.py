282bc55aed14666188a5994e228e3d1c577721e2
GhostyS <40345400+GhostyS@users.noreply.github.com>
2023-05-09 15:34:04 +0300

    Update __main__.py

--- scripts/__main__.py
+++ scripts/__main__.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from elasticsearch import Elasticsearch
+import asyncio
 
 app = Flask(__name__)
+app.config.from_object(Config)
 el_s = Elasticsearch('http://elastic:9200')
 print(app)
+loop = asyncio.new_event_loop()
 
