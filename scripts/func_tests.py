d0a3c73b68aa30c3761d2bdaa1be148b5f966464
GhostyS <40345400+GhostyS@users.noreply.github.com>
2023-05-26 10:00:15 +0300

    Update func_tests.py

--- scripts/func_tests.py
+++ scripts/func_tests.py
@@ -7,3 +7,16 @@ class Test2(unittest.TestCase):#Test_2
 class Test3(unittest.TestCase):#Test_3
   pass
 
+class Test4(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+
+    def test_get_ids_after_delete(self):
+        self.app.delete("/delete/?id=547")
+        time.sleep(2)
+        response = query_index_by_text('docs', 'святой')
+        assert response == [871, 872]
+
+if __name__ == '__main__':
+    unittest.main()
+
