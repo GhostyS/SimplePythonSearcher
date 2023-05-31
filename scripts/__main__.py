1d70f6bf1b5b6ab3ae63eaaf060c5780d6470de2
GhostyS <40345400+GhostyS@users.noreply.github.com>
2023-05-31 03:20:36 +0300

    Delete scripts directory

--- scripts/__main__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from elasticsearch import Elasticsearch
-import asyncio
-
-app = Flask(__name__)
-app.config.from_object(Config)
-el_s = Elasticsearch('http://elastic:9200')
