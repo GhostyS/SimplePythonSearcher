3da838b73d2eb81cf21b0ffe6f6c876c5e7e8afd
GhostyS <40345400+GhostyS@users.noreply.github.com>
2023-06-02 01:58:10 +0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -3,13 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 from elasticsearch import Elasticsearch
 import asyncio
 
+
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config['JSON_AS_ASCII'] = False
 db = SQLAlchemy(app)
-es = Elasticsearch('http://elastic:9200')
+es = Elasticsearch(hosts=["http://127.0.0.1:9200"])
 loop = asyncio.new_event_loop()
-
-
-if __name__ == "__main__":
-    app.run()
 
