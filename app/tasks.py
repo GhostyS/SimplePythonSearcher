1648dfe035cb91db197c4caab317de7054e03f22
GhostyS <40345400+GhostyS@users.noreply.github.com>
2023-06-02 02:31:43 +0300

    Update tasks.py

--- app/tasks.py
+++ app/tasks.py
@@ -2,7 +2,16 @@ from app import db, es
 from elastic import delete_by_id, query_index_by_text, query_index_by_id, add_to_index
 
 async def search(Model, text):
-    if not es.indices.exists(index='docs'):
+    ids = query_index_by_text('docs', text)
+    docs = []
+    for i in ids:
+        docs.extend(query_index_by_id('docs', i))
+    docs_sorted = sorted(docs, key=lambda x: x["_source"]["created_date"])
+    texts = [i["_source"]["text"] + i["_source"]["created_date"] for i in docs_sorted]
+    texts = "<pre>\n\n</pre>".join(texts)
+    return texts
+
+    """if not es.indices.exists(index='docs'):
         for post in Model.query.all():
             add_to_index('docs', post)
     if text is None:
@@ -12,7 +21,7 @@ async def search(Model, text):
         result = Model.query.filter(Model.id.in_(ids)).order_by(Model.created_date.desc()).all()
         return {"result": [post.as_dict() for post in result]}
     except Exception as exc:
-        return str(exc)
+        return str(exc)"""
 
 async def delete(Model, id):
     if not es.indices.exists(index='docs'):
