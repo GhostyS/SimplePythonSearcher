fd298c98871c807d13b699b05ad32ae3159d7bcb
GhostyS <40345400+GhostyS@users.noreply.github.com>
2023-06-01 04:11:03 +0300

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -6,12 +6,12 @@ from app.tasks import search, delete
 @app.get('/search/')
 def get_posts():
     text = request.args["text"]
-    result = loop.run_until_complete(search(Docs, text))
+    result = search(Docs, text)
     return result
 
 @app.delete('/delete/')
 def delete_post():
     id = request.args["id"]
-    result = loop.run_until_complete(delete(Docs, id))
+    result = delete(Docs, id)
     return result
 
