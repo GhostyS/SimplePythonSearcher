e0f46f660154113203005a973856c3bc5cd7f492
GhostyS <40345400+GhostyS@users.noreply.github.com>
2023-06-01 04:01:18 +0300

    Update docker-start.sh

--- docker-start.sh
+++ docker-start.sh
@@ -5,7 +5,7 @@
 docker network create elastic
 
 # pull elasticsearch build and run it
-docker run --name elastic --net elastic -d -p 9200:9200 -p 9300:9300 --rm -e "discovery.type=single-node" -e "xpack.security.enabled=false" docker.elastic.co/elasticsearch/elasticsearch:8.2.3
+docker run --name elastic --net elastic -d -p 9200:9200 -p 9300:9300 --rm -e "discovery.type=single-node" -e "xpack.security.enabled=false" docker.elastic.co/elasticsearch/elasticsearch:8.8.0
 
 # build the flask container
 docker build -t sps:latest .
