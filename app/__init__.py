36ea2c707378595c6b3117d0ddc14116f51e7b6b
GhostyS <40345400+GhostyS@users.noreply.github.com>
2023-06-01 04:10:06 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,13 +3,12 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from elasticsearch import Elasticsearch
-import asyncio
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 es = Elasticsearch('http://elastic:9200')
-loop = asyncio.new_event_loop()
+
 
 from app import routes, models
